9b1512d86a5cfbe558983bcba59685483580eaa1
Tincho228 <martin_quintero521@hotmail.com>
2025-03-25 21:42:06 -0300

    Second feature

--- Fitness-App/app/(tabs)/diet.tsx
+++ Fitness-App/app/(tabs)/diet.tsx
@@ -12,12 +12,16 @@ export default function DietTracker() {
   const [inputCalories, setInputCalories] = useState<string>('');
   const [modalVisible, setModalVisible] = useState(false);
 
+  const [caloriesGoal, setCaloriesGoal] = useState<string>('');
+  const [inputCaloriesGoal, setInputCaloriesGoal]= useState<string>(''); // Initializing Goal Calories
+  const [modalGoalVisible, setModalGoalVisible] = useState(false);
+
   const handleDayPress = (index: number) => {
     setSelectedDay(index);
     setInputCalories(calories[index] || '');
     setModalVisible(true);
   };
-
+  // Function that insert values into the selected Array
   const handleSave = () => {
     if (selectedDay !== null) { // Check for valid index
       const newCalories = [...calories];
@@ -25,6 +29,23 @@ export default function DietTracker() {
       setCalories(newCalories);
     }
     setModalVisible(false);
+  };
+
+  // Function to Calculate Total Calories. 
+  const getTotalCalories = () => {
+    return calories
+      .filter((cal) => cal !== null && cal !== '') // Remove empty values
+      .reduce((sum, cal) => sum + Number(cal), 0); // Convert to number and sum
+  };
+
+  // Function to handle Goal of Calories
+  const handleGoalPress = () => {
+    setInputCaloriesGoal(caloriesGoal || '');
+    setModalGoalVisible(true);
+  };
+  const handleGoalSave = () => {
+    setCaloriesGoal(inputCaloriesGoal);
+    setModalGoalVisible(false);
   };
   return (
     <ParallaxScrollView
@@ -40,10 +61,66 @@ export default function DietTracker() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Step 2 - Diet Tracking</ThemedText>
       </ThemedView>
-      <Text style={styles.textParagraph}>Insert the calories you eat each day.</Text>
+      
+      <TouchableOpacity
+        style={{
+          backgroundColor:'#ddd',
+          padding: 20,
+          margin: 5,
+          borderRadius: 10,
+          alignItems: 'center',
+          width: '100%', // Adjust width for better spacing
+        }} 
+        onPress={() => handleGoalPress()} // Function that calls the Modal
+      >
+        <Text style={styles.title}>
+          Your goal is: {caloriesGoal ? `${caloriesGoal} kcal` : 'Not set'}  
+        </Text>
+      </TouchableOpacity>
+
+
+      {/* This Modal is used to insert the Goal of calories */}
+      <Modal visible={modalGoalVisible} animationType="slide" transparent={true}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+          }}
+        >
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: '80%' }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>
+            <Text>Enter your Calories Goal</Text>
+            </Text>
+            <TextInput
+              keyboardType="numeric"
+              placeholder="Calories"
+              value={inputCaloriesGoal}
+              onChangeText={setInputCaloriesGoal}
+              style={{
+                borderWidth: 1,
+                padding: 10,
+                borderRadius: 5,
+                marginBottom: 10,
+              }}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleGoalSave}>
+                <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonCancel}  onPress={() => setModalGoalVisible(false)}>
+                <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Text style={styles.textParagraph}>Set your Calories Goal.</Text>
       <View style={styles.hr} />
       <View style={{ flex: 1, padding: 20, backgroundColor: '#f8f8f8' }}>
 
+      {/* Creating a List with all the components of the Array */}
       <FlatList
         data={Array.from({ length: 14 }, (_, i) => i + 1)}
         numColumns={4} // Display as a grid (7 days per row)
@@ -67,7 +144,7 @@ export default function DietTracker() {
           </TouchableOpacity>
         )}
       />
-
+      {/* This Modal is used to insert the numbers of calories by the user */}
       <Modal visible={modalVisible} animationType="slide" transparent={true}>
         <View
           style={{
@@ -104,8 +181,22 @@ export default function DietTracker() {
         </View>
       </Modal>
     </View>
-
-      
+    <Text style={styles.textParagraph}>Insert the calories you eat each day.</Text>
+    <View style={styles.hr} />
+    <View
+    style={{
+      backgroundColor:'#ddd',
+      padding: 20,
+      margin: 5,
+      borderRadius: 10,
+      alignItems: 'center',
+      width: '100%', // Adjust width for better spacing
+    }} 
+    >
+      <Text style={styles.title}>
+        Total Calories: {getTotalCalories()} kcal   
+      </Text>
+    </View>
     </ParallaxScrollView>
   );
 }
@@ -150,5 +241,10 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
   },
 });
